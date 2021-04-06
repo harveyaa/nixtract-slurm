abc4afcbaa3fd6220c743966cfceb96f6031ff01
harveyaa <annabelle.ahrv@gmail.com>
2021-04-06 14:04:06 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='nixtract-slurm',
+      version='0.1.0',
+      description='A wrapper for nixtract that enables SLURM job submission.',
+      url='https://github.com/harveyaa/nixtract-slurm',
+      author='Annabelle Harvey',
+      author_email='annabelle.harvey@umontreal.ca',
+      license='MIT',
+      packages=['nixtract-slurm'],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
